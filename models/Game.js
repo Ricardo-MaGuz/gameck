4585841fac90048d8f8108fdcd4b3ee25cafa9a8
YeahGab <gabba.prado@gmail.com>
2019-04-29 11:31:31 -0500

    Se agregó modelo de juegos

---
+++ models/Game.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+const gameSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			unique: true,
+		},
+		genre: {
+            type: String,
+            enum: ["Endless Runner", "Music", "Arcade", "Puzzle", "Beat'em Up", "Educational", "Rythm", "Platformer", 
+            "Shoot'em Up", "Shooter", "Action", "Brick Breaker", "Action", "Turn Based Strategy", "Racing"]
+        },
+		description: String, 
+        createdBy: String,
+        gameSource: String,
+	},
+	{
+		timestamps: true,
+		versionKey: false,
+	}
+)
+
+module.exports = mongoose.model("Game", gameSchema)
