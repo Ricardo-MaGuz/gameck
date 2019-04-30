20bf4d2a72701d6edbd027e5ce74db533c2d7f78
YeahGab <gabba.prado@gmail.com>
2019-04-29 20:07:22 -0500

    Se mejoró la ruta de login (con get y post) y el logout. Se borraron carpetas de views para definir únicamente un dashboard general. Se hizo ruta general en index para dashboard según rol del usuario

--- routes/auth.js
+++ routes/auth.js
@@ -16,52 +16,26 @@ router.post('/signup', (req, res, next) => {
       }
       passport.authenticate('local')(req, res, () => {
         console.log(req.user)
-        return res.redirect('/profile')
+        return res.redirect('/gamers')
       })
     });
   })
 
-        /*passport.authenticate('local')(req, res, ()=>{
-          req.app.locals.loggedUser = user
-          if(req.user.role === 'Admin') return res.redirect('/admins')
-          else if (req.user.role === "Gamer") return res.redirect('/gamers')
-          else if (req.user.role === "Hacker") return res.redirect('/hackers')
-          })
-      }*/
-
 //LOGIN
-router.post('/login', (req, res, next) => {
-    User.register( new User({ username: req.body.username }),
-    req.body.password,
-    function(err, account){
-      if(err){
-        return res.json(err);
-      }
-      return res.redirect('/login')
-    });
-  })
-
-    passport.authenticate('local', (err, user, info) => {
-        if(err) return next(err)
-        if(!user) return res.redirect('index')
-        req.logIn(user, err => {
-            if(err) return next(err)
-            req.app.locals.loggedUser = user
-            if(req.user.role === 'Admin') return res.redirect('/admins')
-            else if (req.user.role === "Gamer") return res.redirect('/gamers')
-                else if (req.user.role === "Hacker") return res.redirect('/hackers')
-        })
-    })
-
+router.get('/', (req, res, next)=>{
+  return res.render('index');
+})
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/dashboard',
+  failureRedirect: '/login'
+}))
 
 //LOGOUT
-/*router.get('/logout', (req, res, next) => {
-    req.app.locals.loggedUser=''
+router.get('/logout', (req, res, next) => {
     req.logOut()
     res.redirect('/login')
 })
 
-
 //PROFILE LOGGED
-router.get('/profile', isLogged, (req, res, next => res.render('auth/profile')))*/
+//router.get('/profile', isLogged, (req, res, next => res.render('auth/profile')))
 module.exports = router
