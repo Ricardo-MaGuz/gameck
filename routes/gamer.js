9e5f63ad011e0020e8921c49cfd7282eeab19bdd
YeahGab <gabba.prado@gmail.com>
2019-04-30 20:33:44 -0500

    Se hizo mini CRUD (RUD) para editar usuarios y/o eliminarlos desde admin con sus respectivas vistas

--- routes/gamer.js
+++ routes/gamer.js
@@ -1,8 +1,65 @@
 const router = require('express').Router()
-const Comment = require('../models/Game')
-const Comment = require('../models/User')
+const User = require('../models/User')
+const Game = require('../models/Game')
 
-router.get('/dashboard/gamer', (req, res, next) => res.render('dashboard/gamer'))
+router.get('/gamer/index', (req, res, next) => res.render('gamer/index'))
 
+router.get('/gamer/games', (req, res, next) => {
+  Game.find()
+    .sort({createdAt: -1})
+    .then(games => {
+      res.render('gamer/games', { games })
+    })
+    .catch(err => next(err))
+})
+
+
+router.get('/gamer/user/edit/:id', (req, res, next) => {
+  const { id } = req.params
+  Game.findById(id)
+    .then(game => {
+      res.render('gamer/user/edit', {game})
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+router.get('/gamer/user/user-details/:id', (req,res) => {
+  const {id} = req.params
+  Game.findById(id)
+  .then(game => {
+    res.render('gamer/users/user-details', {game})
+  })
+  .catch(err => { 
+    res.send(err)
+  })
+})
+
+router.post('/gamer/user/edit/:id', (req, res, next) => {
+  const { id } = req.params
+  Game.findByIdAndUpdate(id, { $set: { ...req.body } }, { new: true })
+    .then(game => {
+      console.log(game)
+      res.redirect(`/gamer/user/user-details/${id}`)
+    })
+    .catch(err => {
+      res.send(err)
+    })
+})
+
+router.get('/gamer/user/delete/:id', (req, res, next) => {
+  const { id } = req.params
+  Game.findByIdAndDelete(id)
+  .then(() => res.redirect('/gamer/user'))
+  .catch(err => next(err))
+})
+
+
+router.get('/gamer/user/delete/:id', (req, res, next) => {
+  const { id } = req.params
+  User.findByIdAndDelete(id)
+  .then(() => res.redirect('/gamer/user'))
+  .catch(err => next(err))
+})
 
 module.exports = router
