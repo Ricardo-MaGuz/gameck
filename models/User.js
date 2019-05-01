08a9ca8b144abe84a07710fbacbb4aa821d3c93a
YeahGab <gabba.prado@gmail.com>
2019-05-01 16:10:50 -0500

    Up to date

--- models/User.js
+++ models/User.js
@@ -43,4 +43,4 @@ const userSchema = new mongoose.Schema(
 )
 
 userSchema.plugin(PLM, {usernameField: "email" })
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model("User", userSchema) 
