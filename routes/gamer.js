24726d1c660149aa905395348dfad642ae8e824d
YeahGab <gabba.prado@gmail.com>
2019-05-01 19:11:18 -0500

    Se intentó hacer query push de mongo para añadir juegos a favoritos y se cambió vista de gamer para revisar que todo quedara bien, aún faltan cambios en esto.

--- routes/gamer.js
+++ routes/gamer.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const User = require('../models/User')
 const Game = require('../models/Game')
+const { isLogged} = require('../handlers/middlewares')
 
-router.get('/dashboard/', (req, res, next) => res.render('dashboard/Gamer'))
+router.get('/dashboard', (req, res, next) => res.render('dashboard/Gamer'))
 
 //CRUD USERS
 
@@ -10,12 +11,12 @@ router.get('/dashboard/', (req, res, next) => res.render('dashboard/Gamer'))
 router.get('/dashboard/edit/:id', (req, res, next) => {
   const { id } = req.params
   User.findById(id)
-    .then(user => {
-      res.render('dashboard/edit', {user})
-    })
-    .catch(err => {
-      console.log(err)
-    })
+  .then(user => {
+    res.render('dashboard/edit', {user})
+  })
+  .catch(err => {
+    console.log(err)
+  })
 })
 
 router.get('/dashboard/edit/:id', (req,res) => {
@@ -32,13 +33,13 @@ router.get('/dashboard/edit/:id', (req,res) => {
 router.post('/dashboard/edit/:id', (req, res, next) => {
   const { id } = req.params
   User.findByIdAndUpdate(id, { $set: { ...req.body } }, { new: true })
-    .then(user => {
-      console.log(user)
-      res.redirect(`/dashboard/`)
-    })
-    .catch(err => {
-      res.send(err)
-    })
+  .then(user => {
+    console.log(user)
+    res.redirect(`/dashboard/`)
+  })
+  .catch(err => {
+    res.send(err)
+  })
 })
 
 //DELETE
@@ -48,5 +49,22 @@ router.get('/dashboard/delete/:id', (req, res, next) => {
   .then(() => res.redirect('/'))
   .catch(err => next(err))
 })
+
+// ADD GAMES TO FAVORITES
+
+router.get('/dashboard/gamer', (req, res) => {
+  res.render('/dashboard/gamer')
+})
+
+router.post('/dashboard/gamer', (req, res, next) => {
+  Game.create({...req.body})
+  .then((newGame) => 
+    Game.findOneAndUpdate({user: user.id},{$push:{favoriteGames: newGame}},{new:true})
+    .populate('games')
+    .then(() => {
+      res.redirect('/dashboard/gamer')
+    })
+    .catch(err => res.send(err))
+)})
 
 module.exports = router
